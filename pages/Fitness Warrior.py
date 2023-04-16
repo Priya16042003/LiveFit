2a4a062d3559523e90b685a269dd6a56e71fb930
Priya16042003 <114289262+Priya16042003@users.noreply.github.com>
2023-04-16 16:11:50 +0530

    Update Fitness Warrior.py

--- pages/Fitness Warrior.py
+++ pages/Fitness Warrior.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="LiveFIT", page_icon=":running:", layout="wide")
 # pip install streamlit-chat  
 from streamlit_chat import message
 # openai.api_key=os.environ["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"]
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("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")
 
-openai.api_key ="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"
+openai.api_key ="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"
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine = "text-davinci-003",
